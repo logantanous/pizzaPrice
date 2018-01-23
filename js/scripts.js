a5024c6347bacf94b36f802864635d5427060bfb
Kayla Ondracek and Logan Tanous <logan@>
2018-01-23 09:33:24 -0800

    add age check for users under 12

--- js/scripts.js
+++ js/scripts.js
@@ -19,17 +19,38 @@ Ticket.prototype.fullPrice = function() {
   return price;
 }
 
+function ageCheck(age, rating) {
+  if (age < 17 && rating === "r") {
+    return "you are too young to see this movie"
+  } else {
+    return false;
+  }
+}
 
-// function resetFields() {
-//     $("input#new-first-name").val("");
-//     $("input#new-last-name").val("");
-//     $("input.new-street").val("");
-//     $("input.new-city").val("");
-//     $("input.new-state").val("");
-// }
 
 // user interface logic
 $(document).ready(function() {
+  $("#new-ticket").submit(function(event) {
+    var inputtedMovie = $("#movie option:selected").text();
+    var inputtedTime = parseInt($("input#time").val());
+    var inputtedAge = parseInt($("input#age").val());
+    var movieRating = $("#movie option:selected").attr("class");
+    var newTicket = new Ticket(inputtedMovie, inputtedTime, inputtedAge);
+
+
+
+    if (ageCheck(inputtedAge, movieRating) !== false ) {
+      alert(ageCheck(inputtedAge, movieRating));
+    } else {
+      $("#total").show();
+      $(".movie-output").text(inputtedMovie);
+      $(".time-output").text(inputtedTime);
+      $(".age-output").text(inputtedAge);
+      $(".price-output").text("$" + newTicket.fullPrice());
+    }
+
+    event.preventDefault();
+  });
 
 
 });
