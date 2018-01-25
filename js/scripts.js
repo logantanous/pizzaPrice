00b7b71ba81d3044362f76b6a50542d416e8ac33
Andrew Peterson and Logan Tanous <Guest@Epicodus-7E.local>
2018-01-25 13:33:35 -0800

    add objects to store values or entry

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ var board = { }; // main object
 function Space(xCoord, yCoord) {
   this.xCoord = xCoord;
   this.yCoord = yCoord;
+  this.spotTaken = false;
+  this.character = "";
 }
 
 function Player(character, turn) {
@@ -20,7 +22,7 @@ var yCoordinate = 1;
 Space.prototype.changeX = function() {
     this.xCoord = xCoordinate;
     if (yCoordinate == 1) {
-      xCoordinate++;
+      return xCoordinate++;
     }
 }
 
@@ -28,11 +30,11 @@ Space.prototype.changeY = function() {
     this.yCoord = yCoordinate;
     yCoordinate++;
     if (yCoordinate == 4) {
-      yCoordinate = 1;
+      return yCoordinate = 1;
     }
 }
 
-for (var x = 0; x <= 8; x++) {
+for (var x = 0; x <= 9; x++) {
   board[x] = new Space(x,yCoordinate);
   board[x].changeY();
   board[x].changeX();
@@ -41,17 +43,34 @@ for (var x = 0; x <= 8; x++) {
 
 $(document).ready(function() {
   $(".btn").click(function(){
-    //parse the id so we can match it with its corresponding object
     var myString = $(this).attr("id");
     var myRegexp = /.(.*?)_(.*?)$/;
     var match = myRegexp.exec(myString);
-    //console.log("match 1: "+match[1]+" match 2: "+match[2]);
     var objectNumber = match[2];
     if (match[1] == 1) {}//object = y
-    if (match[1] == 2) {objectNumber = parseInt(objectNumber) + 3}//object = y+3
-    if (match[1] == 3) {objectNumber = parseInt(objectNumber) + 6}//object = y+6
-    
-    console.log(objectNumber);
+    if (match[1] == 2) {objectNumber = parseInt(objectNumber) + 3}
+    if (match[1] == 3) {objectNumber = parseInt(objectNumber) + 6}
+    if (board[objectNumber].spotTaken == true) {
+      $(".result").html("Pick another spot")
+    }
+    else {
+      if (player1.turn == true) {
+        player1.turn = false;
+        player2.turn = true;
+        $(this).append("<p>"+player1.character+"</p>");
+        board[objectNumber].character = player1.character;
+      } else {
+        player1.turn = true;
+        player2.turn = false;
+        $(this).append("<p>"+player2.character+"</p>");
+        board[objectNumber].character = player2.character;
+      }
 
+      board[objectNumber].spotTaken = true;
+
+      //make a prompt to ask for character
+      $(".result").html("");
+      console.log(objectNumber);
+    }
   })
 })
