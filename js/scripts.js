58a8fea24f8bf0cc5e4cfb12853ac25f15b1bece
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-01-26 15:08:11 -0800

    add logic for getting toppings and final price

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,95 @@
-function Pizza(toppings,size) {
-  //this.cheese = cheese;
-  this.toppings = toppings;
-  this.size = size;
-  this.price = "";
+function Pizza() {
+  this.cheese = true;
+  this.toppings = [];
+  this.size;
+  this.price = 0;
+  this.sauce = "";
 }
 
-var pizza1 = new Pizza([],"XL");
+var pizza1 = new Pizza();
+
+Pizza.prototype.changeSauce = function(sauce) {
+  this.sauce = sauce;
+  return sauce;
+}
+
+Pizza.prototype.changeSize = function(size) {
+  this.size = size;
+  return size;
+}
+
+Pizza.prototype.changeCheese = function(cheese) {
+  this.cheese = cheese;
+  return cheese;
+}
 
 Pizza.prototype.changePrice = function() {
   var finalPrice = 0;
-  if (this.size == "XL") {finalPrice += 15}
-  if (this.size == "L") {finalPrice += 13}
-  if (this.size == "M") {finalPrice += 11}
-  if (this.size == "S") {finalPrice += 9}
-  for (var x = 0; x < pizza1.toppings.length; x++) {
-    if (this.toppings[x] == "bacon") {finalPrice += 2}
-    else {finalPrice += 1}
+  if (this.size == "large") {finalPrice += 13}
+  if (this.size == "medium") {finalPrice += 11}
+  if (this.size == "small") {finalPrice += 9}
+  for (var x = 0; x < this.toppings.length; x++) {
+    if (this.toppings[x] == "bacon") {finalPrice = finalPrice + 2}
+    else {finalPrice = finalPrice + 1}
   }
   this.price = finalPrice;
   return finalPrice;
 }
 
 $(document).ready(function() {
+  var steps = 1;
+  $("input[name=topping]").removeAttr('checked');
 
-  // $(".topping").click(function(){
-  //   if($(this).hasClass("checked")) {
-  //     pizza1.toppings.pop($(this).attr("id"));
-  //     $(this).removeClass("checked");
-  //   } else {
-  //     pizza1.toppings.push($(this).attr("id"));
-  //     $(this).addClass("checked");
-  //   }
-  // })
-
-  
-
-  $(".calculate").click(function() {
-
-    alert($("input[name=cheese]:checked").val());
-
-
-    $(".result").html(pizza1.changePrice());
+  $(".next").click(function() {
+    if (steps == 1) {
+      pizza1.changeSize($("input[name=size]:checked").val());
+      $(".step1").hide();
+      $(".step2").show();
+      $("#current").removeAttr('id');
+      $(".on:nth-of-type(2)").attr('id', 'current');
+      steps++;
+      return false;
+    }
+    if (steps == 2) {
+        if ($("input[name=cheese]").is(':checked')) {pizza1.changeCheese(true)}
+        else {
+          pizza1.changeCheese(false);
+          $(".pizza").attr('src', 'img/marinara.png');
+        }
+        $(".step2").hide();
+        $(".step3").show();
+        $("#current").removeAttr('id');
+        $(".on:nth-of-type(3)").attr('id', 'current');
+        steps++;
+        return false;
+    }
+    if (steps == 3) {
+      pizza1.changeSauce($("input[name=sauce]:checked").val());
+      if (pizza1.cheese == false) {$(".pizza").attr('src', "img/"+pizza1.sauce+".png");}
+      $(".step3").hide();
+      $(".step4").show();
+      $("#current").removeAttr('id');
+      $(".on:nth-of-type(4)").attr('id', 'current');
+      steps++;
+      return false;
+    }
+    if (steps == 4) {
+      $(".step4").hide();
+      $(".result").html(
+        "<h6>Your total is: $"+
+        pizza1.changePrice()+
+        "</h6>"
+        );
+      steps++;
+      return false;
+    }
   })
+
+  $("input[name=topping]").click(function() {
+    if ($(this).is(':checked')) {
+      pizza1.toppings.push($(this).val());
+    }
+    else {pizza1.toppings.pop($("input[name=topping]").val)}
+  })
+
 })
