6aa010108200dce14e8cf17536cb0733167509d0
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-01-26 12:32:58 -0800

    add styling of page

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 function Pizza(toppings,size) {
+  //this.cheese = cheese;
   this.toppings = toppings;
   this.size = size;
   this.price = "";
 }
 
-pizza1 = new Pizza(["Pepporoni","cheese","jalapenos","bacon"],"XL");
+var pizza1 = new Pizza([],"XL");
 
 Pizza.prototype.changePrice = function() {
   var finalPrice = 0;
@@ -12,21 +13,33 @@ Pizza.prototype.changePrice = function() {
   if (this.size == "L") {finalPrice += 13}
   if (this.size == "M") {finalPrice += 11}
   if (this.size == "S") {finalPrice += 9}
-
   for (var x = 0; x < pizza1.toppings.length; x++) {
     if (this.toppings[x] == "bacon") {finalPrice += 2}
     else {finalPrice += 1}
   }
-
   this.price = finalPrice;
   return finalPrice;
 }
 
-
 $(document).ready(function() {
 
+  // $(".topping").click(function(){
+  //   if($(this).hasClass("checked")) {
+  //     pizza1.toppings.pop($(this).attr("id"));
+  //     $(this).removeClass("checked");
+  //   } else {
+  //     pizza1.toppings.push($(this).attr("id"));
+  //     $(this).addClass("checked");
+  //   }
+  // })
 
-  $(".calculate").click(function(){
-    $(".result").append(pizza1.changePrice());
+  
+
+  $(".calculate").click(function() {
+
+    alert($("input[name=cheese]:checked").val());
+
+
+    $(".result").html(pizza1.changePrice());
   })
 })
