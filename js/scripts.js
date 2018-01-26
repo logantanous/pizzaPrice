3a7c8b2564a0a34d5878d3a1c8644c2ca78b5954
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-01-26 10:14:15 -0800

    add logic for backend pricing

--- js/scripts.js
+++ js/scripts.js
@@ -1,100 +1,32 @@
-var board = { }; // main object
-var board = [];
-var xCoordinate = 1;
-var yCoordinate = 1;
-
-function Space(xCoord, yCoord) {
-  this.xCoord = xCoord;
-  this.yCoord = yCoord;
-  this.spotTaken = false;
-  this.character = "";
+function Pizza(toppings,size) {
+  this.toppings = toppings;
+  this.size = size;
+  this.price = "";
 }
 
-function Player(character, turn) {
-  this.character = character;
-  this.turn = turn;
-}
+pizza1 = new Pizza(["Pepporoni","cheese","jalapenos","bacon"],"XL");
 
-player1 = new Player("x",true);
+Pizza.prototype.changePrice = function() {
+  var finalPrice = 0;
+  if (this.size == "XL") {finalPrice += 15}
+  if (this.size == "L") {finalPrice += 13}
+  if (this.size == "M") {finalPrice += 11}
+  if (this.size == "S") {finalPrice += 9}
 
-player2 = new Player("o",false);
-
-Space.prototype.changeX = function() {
-    this.xCoord = xCoordinate;
-    if (yCoordinate == 1) {
-      return xCoordinate++;
-    }
-}
-
-Space.prototype.changeY = function() {
-    this.yCoord = yCoordinate;
-    yCoordinate++;
-    if (yCoordinate == 4) {
-      return yCoordinate = 1;
-    }
-}
-
-function checkValues() {
-  for (i = 0; i < 9; i++) {
-    if ($(".a"+i).children("p").text() == "xxx") {
-      return "Winner is X";
-    }
-    if ($(".a"+i).children("p").text() == "ooo") {
-      return "Winner is O";
-    }
+  for (var x = 0; x < pizza1.toppings.length; x++) {
+    if (this.toppings[x] == "bacon") {finalPrice += 2}
+    else {finalPrice += 1}
   }
+
+  this.price = finalPrice;
+  return finalPrice;
 }
+
 
 $(document).ready(function() {
 
-  for (var x = 1; x <= 9; x++) {
-    board[x] = new Space(x,yCoordinate);
-    board[x].changeY();
-    board[x].changeX();
-  }
 
-  $(".btn").click(function(){
-    var myString = $(this).attr("id");
-    var myRegexp = /.(.*?)_(.*?)$/;
-    var match = myRegexp.exec(myString);
-    var objectNumber = match[2];
-    if (match[1] == 1) {}//object = y
-    if (match[1] == 2) {objectNumber = parseInt(objectNumber) + 3}
-    if (match[1] == 3) {objectNumber = parseInt(objectNumber) + 6}
-    if (board[objectNumber].spotTaken == true) {
-      $(".result").html("Pick another spot")
-    }
-    else {
-      if (player1.turn == true) {
-        player1.turn = false;
-        player2.turn = true;
-        $(this).append("<p>"+player1.character+"</p>");
-        board[objectNumber].character = player1.character;
-      }
-      else {
-        player1.turn = true;
-        player2.turn = false;
-        $(this).append("<p>"+player2.character+"</p>");
-        board[objectNumber].character = player2.character;
-      }
-
-      board[objectNumber].spotTaken = true;
-      //console.log(board[objectNumber]);
-      //make a prompt to ask for character
-      $(".result").html("");
-    }
-
-    checkValues();
-    if (checkValues() == "Winner is X" || checkValues() == "Winner is O") {
-      $(".result").html(checkValues());
-    }
-  })
-  $(".reset").on("click", function() {
-    $("p").remove();
-    $(".result").html("");
-    for (var x = 1; x <= 9; x++) {
-      board[x].spotTaken = false;
-      board[x].character = "";
-    }
+  $(".calculate").click(function(){
+    $(".result").append(pizza1.changePrice());
   })
 })
